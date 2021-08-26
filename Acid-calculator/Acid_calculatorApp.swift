c9ec325668b733d5c4e18068eb1f9b7edb546383
Mark <87848716+Drongyna@users.noreply.github.com>
2021-08-26 13:06:42 +0300

    Initial Commit

---
+++ Acid-calculator/Acid_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Acid_calculatorApp.swift
+//  Acid-calculator
+//
+//  Created by Guest User on 26.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct Acid_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Acid-calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Acid-calculator
+//
+//  Created by Guest User on 26.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
